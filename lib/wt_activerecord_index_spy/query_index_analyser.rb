1134b5a4876c44ddc24be281ffd96f365d00b345
Fabio Perrella <fabio.perrella@gmail.com>
2021-03-03 11:24:29 +0100

    Add a comment about the Thread

--- lib/wt_activerecord_index_spy/query_index_analyser.rb
+++ lib/wt_activerecord_index_spy/query_index_analyser.rb
@@ -15,13 +15,16 @@ module WtActiverecordIndexSpy
       # - WHERE lala = 1 AND popo = 1
       # - WHERE lala = 2 AND popo = 2
       return if @analysed_queries.include?(query)
+      @analysed_queries << query
 
+      # We need a thread to use a different connection that it's used by the
+      # application otherwise, it can change some ActiveRecord internal state
+      # such as number_of_affected_rows that is returned by the method
+      # `update_all`
       Thread.new do
-        # more details about the result https://dev.mysql.com/doc/refman/8.0/en/explain-output.html
         results = ActiveRecord::Base.connection_pool.with_connection do |conn|
           conn.query("explain #{query}")
         end
-        @analysed_queries << query
 
         results.find do |result|
           criticality_level = analyse_explain(result)
@@ -45,6 +48,7 @@ module WtActiverecordIndexSpy
       _id, _select_type, _table, _partitions, type, possible_keys, key, _key_len,
         _ref, _rows, _filtered, extra = result
 
+      # more details about the result in https://dev.mysql.com/doc/refman/8.0/en/explain-output.html
       return if type == "ref"
       return if ALLOWED_EXTRA_VALUES.any? { |value| extra&.include?(value) }
 
