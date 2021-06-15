25f6f24e9c6079db5a2c389f89bc9a0d0f5f1dbe
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2021-06-15 17:59:06 +0100

    Rubocop updates

--- spec/lib/wt_activerecord_index_spy/notification_listener_spec.rb
+++ spec/lib/wt_activerecord_index_spy/notification_listener_spec.rb
@@ -15,13 +15,14 @@ RSpec.describe WtActiverecordIndexSpy::NotificationListener do
     expect(aggregator.uncertain_results.count).to eq(0)
   end
 
-  describe 'Identifier not provided by ActiveRecord' do    
-    let(:values) do 
-      { sql: 'SELECT * FROM `users` WHERE `name` = "lala"'}
+  describe 'Identifier not provided by ActiveRecord' do
+    let(:values) do
+      { sql: 'SELECT * FROM `users` WHERE `name` = "lala"' }
     end
 
     it 'defaults to empty string if identifier not provided' do
-      listener = WtActiverecordIndexSpy::NotificationListener.new(ignore_queries_originated_in_test_code: false, aggregator: aggregator)
+      listener = WtActiverecordIndexSpy::NotificationListener.new(ignore_queries_originated_in_test_code: false,
+                                                                  aggregator: aggregator)
       listener.call('name', 'start', 'finish', 'message_id', values)
       expect(aggregator.certain_results.count).to eq 1
       expect(aggregator.certain_results.first.identifier).to eq ''
