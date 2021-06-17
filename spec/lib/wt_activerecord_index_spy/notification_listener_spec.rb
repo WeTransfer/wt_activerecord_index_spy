6b5f8644f384555eb5877153dd5754f604ab1239
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2021-06-17 09:02:28 +0100

    Add separate mysql/postgresql tests

--- spec/lib/wt_activerecord_index_spy/notification_listener_spec.rb
+++ spec/lib/wt_activerecord_index_spy/notification_listener_spec.rb
@@ -21,12 +21,23 @@ RSpec.describe WtActiverecordIndexSpy::NotificationListener do
       { sql: query }
     end
 
-    it 'defaults to empty string if identifier not provided' do
+  describe 'No identifier provided' do
+    before(:each) do
       listener = WtActiverecordIndexSpy::NotificationListener.new(ignore_queries_originated_in_test_code: false,
-                                                                  aggregator: aggregator)
+      aggregator: aggregator)
       listener.call('name', 'start', 'finish', 'message_id', values)
+    end
+
+    it 'defaults to empty string if identifier not provided (mysql)', only: [:mysql2] do
+      expect(aggregator.uncertain_results.count).to eq 0
       expect(aggregator.certain_results.count).to eq 1
       expect(aggregator.certain_results.first.identifier).to eq ''
+    end
+
+    it 'defaults to empty string (postgresql)', only: [:postgresql] do
+      expect(aggregator.certain_results.count).to eq 0
+      expect(aggregator.uncertain_results.count).to eq 1
+      expect(aggregator.uncertain_results.first.identifier).to eq ''
     end
   end
 
