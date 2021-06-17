1017ec93e1b9a0f4c4dfbd2362d826cadfac199e
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2021-06-17 11:31:07 +0100

    Fix issue with missing identifier from ActiveRecord (#21)
    If ActiveRecord does not provide an identifier this causes the output to fail. This update simply stringifies the identifier so a crash does not happen.

--- spec/lib/wt_activerecord_index_spy/notification_listener_spec.rb
+++ spec/lib/wt_activerecord_index_spy/notification_listener_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe WtActiverecordIndexSpy::NotificationListener do
-  it "ignores queries to INFORMATION_SCHEMA", only: [:mysql2] do
-    aggregator = WtActiverecordIndexSpy::Aggregator.new
+  let(:aggregator) { WtActiverecordIndexSpy::Aggregator.new }
+  let(:query) { "SELECT * from users where name like 'lala%'" }
 
+  it "ignores queries to INFORMATION_SCHEMA", only: [:mysql2] do
     WtActiverecordIndexSpy.watch_queries(aggregator: aggregator, ignore_queries_originated_in_test_code: false) do
       ActiveRecord::Base.connection.execute(
         "SELECT count FROM INFORMATION_SCHEMA.INNODB_METRICS "\
@@ -15,15 +16,34 @@ RSpec.describe WtActiverecordIndexSpy::NotificationListener do
     expect(aggregator.uncertain_results.count).to eq(0)
   end
 
+  describe 'Identifier not provided by ActiveRecord' do
+    let(:values) do
+      { sql: query }
+    end
+
+    before(:each) do
+      listener = WtActiverecordIndexSpy::NotificationListener.new(ignore_queries_originated_in_test_code: false, aggregator: aggregator)
+      listener.call('name', 'start', 'finish', 'message_id', values)
+    end
+
+    it 'defaults to empty string if identifier not provided (mysql)', only: [:mysql2] do
+      expect(aggregator.uncertain_results.count).to eq 0
+      expect(aggregator.certain_results.count).to eq 1
+      expect(aggregator.certain_results.first.identifier).to eq ''
+    end
+
+    it 'defaults to empty string (postgresql)', only: [:postgresql] do
+      expect(aggregator.certain_results.count).to eq 0
+      expect(aggregator.uncertain_results.count).to eq 1
+      expect(aggregator.uncertain_results.first.identifier).to eq ''
+    end
+  end
+
   context "when the adapter is mysql2" do
     it "does not ignore queries with empty identifier", only: [:mysql2] do
-      aggregator = WtActiverecordIndexSpy::Aggregator.new
       User.create!(name: "lala")
-
       WtActiverecordIndexSpy.watch_queries(aggregator: aggregator, ignore_queries_originated_in_test_code: false) do
-        ActiveRecord::Base.connection.execute(
-          "SELECT * from users where name like 'lala%'"
-        )
+        ActiveRecord::Base.connection.execute(query)
       end
 
       expect(aggregator.certain_results.count).to eq(1)
@@ -33,13 +53,10 @@ RSpec.describe WtActiverecordIndexSpy::NotificationListener do
 
   context "when the adapter is postgresql" do
     it "does not ignore queries with empty identifier", only: [:postgresql] do
-      aggregator = WtActiverecordIndexSpy::Aggregator.new
       User.create!(name: "lala")
 
       WtActiverecordIndexSpy.watch_queries(aggregator: aggregator, ignore_queries_originated_in_test_code: false) do
-        ActiveRecord::Base.connection.execute(
-          "SELECT * from users where name like 'lala%'"
-        )
+        ActiveRecord::Base.connection.execute(query)
       end
 
       expect(aggregator.certain_results.count).to eq(0)
